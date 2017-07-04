805ec070567d4bf82f449d83f22255b752869ecb
ab197 <ab197@hdm-stuttgart.de>
2017-07-04 12:32:56 +0200

    kakaka

---
+++ Wersindwir.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>The Society Explorer</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- Bootstrap -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" />
+    <style>
+
+
+        html {
+
+        }
+
+        body {
+            padding-top: 20px;
+            font-size: 16px;
+            font-family: "Futura";
+            backgroud: transparent;
+
+            background: url(http://www.lightsniper.de/wordpress/wp-content/gallery/hasenberg/DSC_3309-Panorama.jpg) no-repeat center center fixed;
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            -o-background-size: cover;
+            background-size: cover;
+        }
+        h1 {
+            font-family: "Futura";
+            font-weight: 400;
+            font-size: 32px;
+        }
+
+        .panel {
+            background-color: rgba(255, 255, 255, 0.7532);
+        }
+
+        .margin-base-vertical {
+            margin: 40px 0;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-md-offset-4 col-md-4 col-sm-offset-3 col-sm-6">
+            <div class="panel" style="padding: 20px">
+                <h1 class="margin-base-vertical">The Society Explorer</h1>
+                <p>
+                    The Society Explorer is your leading and innovative social media browser!<br>
+                </p>
+                <p>
+                    You are able to connect with friends and share your most beautiful moments<br>
+                    through different types of tweets!
+                </p>
+                <p>
+                    Emma Watson: "The Society Explorer changed my live! Thank you for making the social world great again!
+                </p>
+                <p> Johhny Deep: "The SE got me new power to stay up-to-date with my friends!</p>
+                <p><h1>What about you? Become part of the family!</h1></p>
+               <br>
+                <button type="link" class="btn btn-default btn-md" onclick="location='Registrierung.php'">Register</button>
+
+                    </span>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
